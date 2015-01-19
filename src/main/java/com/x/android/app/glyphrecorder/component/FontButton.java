e98aff81c0e35991d9f91167b512e3cfb4997a4d
KyokuX <developer.tcx@gmail.com>
2015-01-19 12:09:56 +0800

    修改文字大小问题
    根据屏幕最小宽度计算并设置文字大小

--- src/main/java/com/x/android/app/glyphrecorder/component/FontButton.java
+++ src/main/java/com/x/android/app/glyphrecorder/component/FontButton.java
@@ -1,19 +1,29 @@
 package com.x.android.app.glyphrecorder.component;
 
 import android.content.Context;
+import android.graphics.Point;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.xi47.common.android.util.DeviceUtil;
 
 /**
  * Created by X on 14/11/29.
  */
 public class FontButton extends Button {
 
+    private static final int SCALE_RATE_TEXT_SIZE = 13;
+
     public FontButton(Context context, AttributeSet attrs) {
         super(context, attrs);
         Typeface font = Typeface.createFromAsset(getContext().getAssets(), "fonts/coda.ttf");
         setTypeface(font);
+        DeviceUtil.getInstance().init(context);
+        Point screenSize = DeviceUtil.getInstance().getDisPlayPoint();
+        int miniSize = screenSize.x > screenSize.y ? screenSize.y : screenSize.x;
+        setTextSize(TypedValue.COMPLEX_UNIT_PX, miniSize / SCALE_RATE_TEXT_SIZE);
     }
 }
